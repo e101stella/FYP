08cd9bb7b1999180c8c3a8990e122bd74ab5b047
e101stella <e101stellar@gmail.com>
2022-03-28 15:35:32 +1100

    Updating threaded BCM

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ class Program{
         // Generator.Generator.GenerateNewMatrix(1, 10000, 5, 0, 1); 
 
         Matrix<double> input = SparseIO.Reader("TestCases/1n1000_d5_m0_std1.csv");
-        Matrix<double> output = BCM.BCM.Solver(-input);
+        Matrix<double> output = ThreadedBCM.BCM.Solver(-input);
 
         double opt_sol = input.PointwiseMultiply(output * output.Transpose()).RowSums().Sum(); 
         Console.Write(opt_sol);
